fe01b707e44868afc8daec4a3c1a2603e1e6c4a1
ioannissp <146840916+ioannissp@users.noreply.github.com>
2024-09-14 16:04:42 +0300

    Update make_missing_mar.R

--- src/missing-data-functions/make_missing_mar.R
+++ src/missing-data-functions/make_missing_mar.R
@@ -1,7 +1,7 @@
 ##### Missingness Generating Function #####
 make_missing<-function(data, run){
   set.seed(seed=run)
-  probmis<-sapply(data$x, function(chi) plogis(-1+chi)) #probability of each datum missing
+  probmis<-sapply(data$x, function(chi) plogis(0.5+chi)) #probability of each datum missing
   ry<-sapply(probmis, function(p) rbinom(1,1,p))
   data[ry==1, "y"]<-NA
   return(data)
